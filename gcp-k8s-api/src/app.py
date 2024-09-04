e96c5f3048f523499a3a13f84bb43d6aba65837a
Remscode <remiabigeal17@gmail.com>
2024-09-04 16:23:22 +0100

    Add deployment configurations and API code

--- gcp-k8s-api/src/app.py
+++ gcp-k8s-api/src/app.py
@@ -1,14 +1,24 @@
-#import modules
-from flask import flask , jsonify
+from flask import Flask, jsonify
 #get current date module
-from  datetime import datetime
+from datetime import datetime
+import pytz  # Import pytz for time zone handling
+
 #create a flask instance class
-app = flask(__name__)
+app = Flask(__name__)
+
 #define the api route
 @app.route('/time', methods=['GET'])
 #Executes the api routes
 def get_current_time():
-    return jsonify({"current_time" :datetime.utcnow().isoformat()+ 'z'})
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # Set the time zone for Nigeria
+    timezone = pytz.timezone('Africa/Lagos')  # Nigeria's time zone
+    current_time = datetime.now(timezone).isoformat() + 'Z'
+    return jsonify({"current_time": current_time})
+
+#  error handler for 404 errors
+# @app.errorhandler(404)
+# def not_found(error):
+#     return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
